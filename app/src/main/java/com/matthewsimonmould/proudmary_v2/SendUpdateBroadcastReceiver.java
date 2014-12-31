a0a0d66de950d3524bb474e2a065594dfb6ad5cc
etp404 <L1lliput>
2014-12-31 18:31:10 +0000

    Test drove out wiring up of broadcast receiver and service.

--- app/src/main/java/com/matthewsimonmould/proudmary_v2/SendUpdateBroadcastReceiver.java
+++ app/src/main/java/com/matthewsimonmould/proudmary_v2/SendUpdateBroadcastReceiver.java
@@ -1,17 +1,18 @@
 package com.matthewsimonmould.proudmary_v2;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class SendUpdateBroadcastReceiver extends BroadcastReceiver {
-    private final NotificationManagerWrapper notificationManagerWrapper;
-    public SendUpdateBroadcastReceiver(NotificationManagerWrapper notificationManagerWrapper) {
-        this.notificationManagerWrapper = notificationManagerWrapper;
+
+    public SendUpdateBroadcastReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        notificationManagerWrapper.provideNotification("Something happened");
+        Intent serviceIntent = new Intent(context, SendNotificationService.class);
+        context.startService(serviceIntent);
     }
 }
