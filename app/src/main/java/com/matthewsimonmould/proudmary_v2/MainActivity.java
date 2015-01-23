d187f51db6fd89caf2a56332cb80ba6f9095e9c5
mattmould-bbc <matthew.mould@bbc.co.uk>
2015-01-23 12:21:07 +0000

    Contact picker.

--- app/src/main/java/com/matthewsimonmould/proudmary_v2/MainActivity.java
+++ app/src/main/java/com/matthewsimonmould/proudmary_v2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.matthewsimonmould.proudmary_v2;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -14,8 +20,10 @@ import com.matthewsimonmould.proudmary_v2.uifields.RecipientTextField;
 
 
 public class MainActivity extends ActionBarActivity {
+	static final int PICK_CONTACT_REQUEST = 0;
+	private RecipientTextField recipientTextField;
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,9 +39,19 @@ public class MainActivity extends ActionBarActivity {
 		frequencyTextField.setTextField(updaterSettings.getUpdatePeriodInMinutes());
 		frequencyTextField.setEnabledOrDisabledAccordingToUpdateStatus(); //TODO: set this to picker.
 
-		RecipientTextField recipientTextField = new RecipientTextField((EditText)findViewById(R.id.recipientNumber), storedUpdateSetting);
+		recipientTextField = new RecipientTextField((EditText)findViewById(R.id.recipientNumber), storedUpdateSetting);
 		recipientTextField.setTextField(updaterSettings.getRecipient());
 		recipientTextField.setEnabledOrDisabledAccordingToUpdateStatus();
+
+		Button contactPickerButton = (Button)findViewById(R.id.button_contact_picker);
+		contactPickerButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+				startActivityForResult(intent, PICK_CONTACT_REQUEST);
+
+			}
+		});
 
 		DestinationTextField destinationTextField = new DestinationTextField((EditText)findViewById(R.id.destination), storedUpdateSetting);
 		destinationTextField.setTextField(updaterSettings.getDestination());
@@ -42,4 +60,17 @@ public class MainActivity extends ActionBarActivity {
 		StartSwitchListener startSwitchListener = new StartSwitchListener(getApplicationContext(), storedUpdateSetting, updaterSettings, destinationTextField, frequencyTextField, recipientTextField);
 		switchButton.setOnCheckedChangeListener(startSwitchListener);
     }
+
+	protected void onActivityResult(int requestCode, int resultCode,
+									Intent data) {
+		if (requestCode == PICK_CONTACT_REQUEST) {
+			if (resultCode == RESULT_OK) {
+				Uri uri = data.getData();
+				Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+				cursor.moveToFirst();
+				int  phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+				recipientTextField.setTextField(cursor.getString(phoneIndex));
+			}
+		}
+	}
 }
