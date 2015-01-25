8fdb8ce0a5be51a53e92c2691632b38172e398b3
mattmould-bbc <matthew.mould@bbc.co.uk>
2015-01-25 15:20:43 +0000

    If cannot get location task is scheduled for five minutes and error notification is provided.

--- app/src/main/java/com/matthewsimonmould/proudmary_v2/Notifier.java
+++ app/src/main/java/com/matthewsimonmould/proudmary_v2/Notifier.java
@@ -8,24 +8,25 @@ import android.content.Intent;
 import android.content.res.Resources;
 
 public class Notifier {
-    private Context context;
+	private final NotificationManager notificationManager;
+	private Context context;
 
-    public Notifier(Context context) {
+	public Notifier(Context context) {
         this.context = context;
-    }
+		this.notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+	}
 
     public void notify(String message) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
         Resources resources = context.getResources();
-        Notification.Builder notificationBuilder = new Notification.Builder(context)
-                .setContentTitle(resources.getString(R.string.app_name))
-                .setContentText(message)
-                .setDefaults(Notification.DEFAULT_LIGHTS)
-                .setSmallIcon(R.drawable.pm_logo)
-                .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0));
+		Notification.Builder notificationBuilder = new Notification.Builder(context)
+				.setContentTitle(resources.getString(R.string.app_name))
+				.setContentText(message)
+				.setDefaults(Notification.DEFAULT_LIGHTS)
+				.setSmallIcon(R.drawable.pm_logo)
+				.setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0));
 
         Notification notification = notificationBuilder.build();
-        notificationManager.notify(0, notification);
-    }
+		notificationManager.notify(0, notification);
+	}
+
 }
--- app/src/main/java/com/matthewsimonmould/proudmary_v2/UpdaterService.java
+++ app/src/main/java/com/matthewsimonmould/proudmary_v2/UpdaterService.java
@@ -19,86 +19,97 @@ import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListe
 import com.matthewsimonmould.proudmary_v2.settings.UpdaterSettings;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class UpdaterService extends Service implements ConnectionCallbacks, OnConnectionFailedListener {
 
+	private long retryIfFailDelay = TimeUnit.MINUTES.toMillis(5);
 	private String debugTag;
-    private GoogleApiClient mGoogleApiClient;
+	private GoogleApiClient mGoogleApiClient;
 
-    @Override
-    public void onCreate() {
+	@Override
+	public void onCreate() {
 		debugTag = getClass().getSimpleName();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(LocationServices.API)
+				.build();
+	}
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (mGoogleApiClient.isConnected()) {
-            sendUpdate();
-        }
-        else {
-            mGoogleApiClient.connect();
-        }
-        return 0;
-    }
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if (mGoogleApiClient.isConnected()) {
+			sendUpdate();
+		} else {
+			mGoogleApiClient.connect();
+		}
+		return 0;
+	}
 
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
 
-    @Override
-    public void onConnected(Bundle bundle) {
+	@Override
+	public void onConnected(Bundle bundle) {
 		Log.d(debugTag, "onConnected");
 		sendUpdate();
-    }
+	}
 
-    private void sendUpdate() {
+	private void sendUpdate() {
 		AsyncTask<Void, Void, Void> asyncUpdateTask = new AsyncTask<Void, Void, Void>() {
 
 			@Override
 			protected Void doInBackground(Void... params) {
+				Notifier notifier = new Notifier(getApplicationContext());
 				UpdaterSettings updaterSettings = new UpdaterSettings(getApplicationContext().getSharedPreferences(UpdaterSettings.UPDATER_SETTINGS, 0));
 				Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-				//TODO: validate last location.
-				GoogleMapsDurationGetter googleMapsDurationGetter = new GoogleMapsDurationGetter(new UrlAccessor());
-				MessageGenerator messageGenerator = new MessageGenerator(googleMapsDurationGetter);
-				String message = messageGenerator.generateMessage(
-						String.valueOf(lastLocation.getLatitude()),
-						String.valueOf(lastLocation.getLongitude()),
-						updaterSettings.getDestination());
-				SMSSender.sendSMS(updaterSettings.getRecipient(), message);
-				new Notifier(getApplicationContext()).notify(getApplicationContext().getResources().getString(R.string.update_sent_notification));
+				if (lastLocation != null) {
+					GoogleMapsDurationGetter googleMapsDurationGetter = new GoogleMapsDurationGetter(new UrlAccessor());
+					MessageGenerator messageGenerator = new MessageGenerator(googleMapsDurationGetter);
+					String message = messageGenerator.generateMessage(
+							String.valueOf(lastLocation.getLatitude()),
+							String.valueOf(lastLocation.getLongitude()),
+							updaterSettings.getDestination());
 
-				Intent intent = new Intent(getApplicationContext(), SendUpdateBroadcastReceiver.class);
-				PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+					SMSSender.sendSMS(updaterSettings.getRecipient(), message);
 
-				AlarmManager alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-				alarmManager.cancel(pendingIntent);
+					notifier.notify(getApplicationContext().getResources().getString(R.string.update_sent_notification));
 
-				alarmManager.set(AlarmManager.RTC_WAKEUP, new Date().getTime() + updaterSettings.getUpdatePeriodInMillis(), pendingIntent);
+					scheduleNextUpdate(updaterSettings.getUpdatePeriodInMillis());
+				} else {
+					notifier.notify(getApplicationContext().getResources().getString(R.string.update_could_not_be_sent));
 
+					scheduleNextUpdate(retryIfFailDelay);
+				}
 				return null;
 			}
 		};
+
 		asyncUpdateTask.execute();
 	}
 
+	private void scheduleNextUpdate(Long delay) {
+		Intent intent = new Intent(getApplicationContext(), SendUpdateBroadcastReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+		AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+		alarmManager.cancel(pendingIntent);
+
+		alarmManager.set(AlarmManager.RTC_WAKEUP, new Date().getTime() + delay, pendingIntent);
+	}
 
 
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(debugTag, "UpdaterService.onConnectionSuspended"); //TODO: handle this.
-    }
+	@Override
+	public void onConnectionSuspended(int i) {
+		Log.d(debugTag, "UpdaterService.onConnectionSuspended"); //TODO: handle this.
+	}
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d(debugTag, "UpdaterService.onConnectionFailed"); //TODO: handle this.
-    }
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		Log.d(debugTag, "UpdaterService.onConnectionFailed"); //TODO: handle this.
+	}
 }
