97be2a1effe8c9dd77f8a96303fc6af0d90698c7
etp404 <L1lliput>
2015-03-01 11:47:15 +0000

    Fleshed out address validator.

--- purejava/src/main/java/com/khonsu/enroute/AddressValidator.java
+++ purejava/src/main/java/com/khonsu/enroute/AddressValidator.java
@@ -1,12 +1,18 @@
 package com.khonsu.enroute;
 
+import java.util.List;
+
 public class AddressValidator {
+
+	private GooglePlacesAutocompleter googlePlacesAutocompleter;
 
 	public AddressValidator(GooglePlacesAutocompleter googlePlacesAutocompleter) {
 
+		this.googlePlacesAutocompleter = googlePlacesAutocompleter;
 	}
 
-	public static boolean validate(String someAddress) {
-		return true;
+	public boolean validate(String someAddress) {
+		List<String> autcompleterResults = googlePlacesAutocompleter.getSuggestions(someAddress);
+		return  autcompleterResults!= null && autcompleterResults.size() > 0;
 	}
 }
--- purejava/src/test/java/com/khonsu/enroute/AddressValidatorTest.java
+++ purejava/src/test/java/com/khonsu/enroute/AddressValidatorTest.java
@@ -3,29 +3,52 @@ package com.khonsu.enroute;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AddressValidatorTest {
+
+	private String validAddress = "52 Some Valid Address";
+	private String invalidAddress = "52 Some invalid Address";
+
 	@Test
 	public void testThatCorrectAddressIsValidatedAsTrue() {
-		String someAddress = "52 Some Valid Address";
-		AddressValidator addressValidator = new AddressValidator(new FakeGooglePlacesAutocompleter(someAddress));
-		assertTrue(addressValidator.validate(someAddress));
+		AddressValidator addressValidator = new AddressValidator(new FakeGooglePlacesAutocompleter());
+		assertTrue(addressValidator.validate(validAddress));
+	}
+
+	@Test
+	public void testThatAddressIsValidatedAsFalseIfNullResultsAreResultsFromTheAutocompleter() {
+		AddressValidator addressValidator = new AddressValidator(new FakeGooglePlacesAutocompleter());
+		assertFalse(addressValidator.validate("anything"));
+	}
+
+	@Test
+	public void testThatAddressIsValidatedAsFalseIfZeroResultsAreResultsFromTheAutocompleter() {
+		AddressValidator addressValidator = new AddressValidator(new FakeGooglePlacesAutocompleter());
+		assertFalse(addressValidator.validate(invalidAddress));
 	}
 
 	public class FakeGooglePlacesAutocompleter extends GooglePlacesAutocompleter {
-		private final List<String> results;
+		private final Map<String, List<String>> queryToResults =
+				new HashMap<String, List<String>>(){{
+					this.put(validAddress, new ArrayList<String>() {{
+						this.add(validAddress);
+					}});
+					this.put(invalidAddress, new ArrayList<String>());
+				}};
 
-		public FakeGooglePlacesAutocompleter(final String someAddress) {
+		public FakeGooglePlacesAutocompleter() {
 			super(null);
-			results = new ArrayList<String>(){{this.add(someAddress);}};
 		}
 
 		@Override
 		public List<String> getSuggestions(String location) {
-			return results;
+			return queryToResults.get(location);
 		}
 	}
 }
