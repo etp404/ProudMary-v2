6e5169bf138a71f0d7437d9937b251200e9b600f
Matt Mould <matthew.mould@bbc.co.uk>
2015-04-03 14:21:52 +0100

    Contact can parse and format.

--- purejava/src/test/java/com/khonsu/enroute/ContactTest.java
+++ purejava/src/test/java/com/khonsu/enroute/ContactTest.java
@@ -5,12 +5,24 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ContactTest {
+
 	@Test
-	public void testThatFormatPerformsAsExpected() {
-		String name = "Brian Singer";
+	public void testThatCanParseStringWithoutNameAndOutputNumber() {
 		String number = "0123456";
 
-		Contact contact = new Contact(name, number);
-		assertEquals(name + " <" + number + ">", contact.toString());
+		Contact contact = Contact.fromString(number);
+		assertEquals(number, contact.toString());
+		assertEquals(number, contact.getNumber());
+	}
+
+	@Test
+	public void testThatCanParseStringWithNameAndOutputNumber() {
+		String name = "Brian Singer";
+		String number = "0123456";
+		String contactString = name + " <" + number + ">";
+
+		Contact contact = Contact.fromString(contactString);
+		assertEquals(contactString, contact.toString());
+		assertEquals(number, contact.getNumber());
 	}
 }
