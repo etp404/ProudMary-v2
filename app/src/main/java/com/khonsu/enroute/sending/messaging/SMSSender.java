9de4f9f9f375cda2fe8d592392a73c609e75aa40
Matt Mould <matthew.mould@bbc.co.uk>
2015-05-03 16:11:55 +0100

    Support for long messages.

--- app/src/main/java/com/khonsu/enroute/sending/messaging/SMSSender.java
+++ app/src/main/java/com/khonsu/enroute/sending/messaging/SMSSender.java
@@ -2,9 +2,17 @@ package com.khonsu.enroute.sending.messaging;
 
 import android.telephony.SmsManager;
 
+import java.util.ArrayList;
+
 public class SMSSender {
     public static void sendSMS(String recipientNumber, String message) {
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(recipientNumber, null, message, null, null);
+        ArrayList<String> parts = sms.divideMessage(message);
+        if (parts.size() == 1) {
+            sms.sendTextMessage(recipientNumber, null, message, null, null);
+        }
+        else {
+            sms.sendMultipartTextMessage(recipientNumber, null, parts, null, null);
+        }
     }
 }
